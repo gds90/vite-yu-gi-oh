6a354c6c6f9210a08532a840856a353bc7993c01
gds90 <you@example.com>
2024-01-10 14:34:37 +0100

    Added AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,30 @@
 <script>
+import { store } from '../store'
 export default {
+    data() {
+        return {
+            store
+        }
+    },
     name: 'AppHeader'
 }
 </script>
 <template lang="">
     <header>
-        <div class="container">
-            PROVA PROVA
+        <div class="container d-flex my-2 ms-2">
+            <img id="logo" src="/public/Yu-Gi-Oh!.png" alt="Yu-Gi-Oh!">
+            <h1 class="ms-3">{{store.app_title}}</h1>
         </div>
     </header>
 </template>
 <style lang="scss">
+@use '../styles/generals.scss';
+
 header {
     height: 75px;
-    background-color: beige;
+}
+
+#logo {
+    width: 100px
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-    // inserire variabili generiche
+    app_title: 'Yu-Gi-Oh Api'
 });
